c2c828c3e27ee853a91c2688ef6d730e4d28659f
mattiapennisi <mattiapennisi0@gmail.com>
2025-06-17 21:25:17 +0200

    Refactor password generation logic to improve character pool handling and streamline password creation

--- functions.php
+++ functions.php
@@ -2,20 +2,38 @@
 
 function generateRandomPassword()
 {
-    $allCharacters = '!#$%&()*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    $passwordGenerated = '';
+    $lettersPool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    $numbersPool = '0123456789';
+    $symbolsPool = '!#$%&()*';
+    $allCharactersPool = '';
+    $generatedPassword = '';
+
+    if (isset($_GET['allowForLetters'])) {
+        $allCharactersPool .= $lettersPool;
+    }
+
+    if (isset($_GET['allowForNumbers'])) {
+        $allCharactersPool .= $numbersPool;
+    }
+
+    if (isset($_GET['allowForSymbols'])) {
+        $allCharactersPool .= $symbolsPool;
+    }
+
+    if (empty($allCharactersPool)) {
+        $allCharactersPool = $lettersPool . $numbersPool . $symbolsPool;
+    }
 
     if (isset($_GET['passwordLength'])) {
-        $passwordGenerated = '';
-        $length = intval($_GET['passwordLength']);
+        $passwordLength = intval($_GET['passwordLength']);
 
-        for ($i = 0; $i < $length; $i++) {
-            $randomIndex = rand(0, strlen($allCharacters) - 1);
-            $passwordGenerated .= $allCharacters[$randomIndex];
+        for ($i = 0; $i < $passwordLength; $i++) {
+            $randomIndex = rand(0, strlen($allCharactersPool) - 1);
+            $generatedPassword .= $allCharactersPool[$randomIndex];
         }
 
         header('Location: result.php');
-
-        return $passwordGenerated;
     }
+
+    return $generatedPassword;
 };
